c6c7bda363471a49dc70f343bc682c17f6bdbe58
shivampip <shivam301296@gmail.com>
2019-05-20 15:14:16 +0530

    dist added

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="dsolver",
+    version="0.0.1",
+    author="Shivam Agrawal",
+    author_email="shivam301296@gmail.com",
+    description="Resolve Python dependencies automatically",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/shivampip/DSolver",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
